518bf7553b68d6e4d253f317e16ae67bf1040d9a
Le Tuan Anh <letuananh035@gmail.com>
2019-01-03 15:59:36 +0700

    Init demo

---
+++ app/src/main/java/com/validation/demo/demovalidation/Custom/Customer.java
@@ -0,0 +1,42 @@
+package com.validation.demo.demovalidation.Custom;
+
+import core.IValidation;
+import core.annotation.MinDouble;
+import core.annotation.NotEmpty;
+import core.annotation.NotNull;
+import core.annotation.Regex;
+import core.annotation.Size;
+import core.result.ResultItemObserver;
+
+public class Customer extends IValidation
+{
+
+    @NotEmpty
+    public String name;
+    @NotNull
+    @NotEmpty
+    @Regex(regex = "\\d+")
+    public String phone;
+
+    @MinDouble(min = 1)
+    public int age;
+
+    @NotEmpty
+    @Email
+    public String email;
+
+    //@NotNull
+    //@MinDouble(min = 2, target = "$.age",message = "Ban cua ban cua ban qua nho")
+    //public Customer friend;
+
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    protected void setupValidation(ResultItemObserver resultItemObserver) {
+
+    }
+}
+
